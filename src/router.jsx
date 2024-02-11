0053e73a57050fa1a22a48a8ae82ca491a6ab4ee
Anderson Almeida <andersonpa10pa@gmail.com>
2024-02-11 10:24:23 -0300

    Configuração inicial das rotas

---
+++ src/router.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from './pages/Home'
+import { ItemInStock } from "./pages/ItemInStock";
+import { NewItem } from "./pages/NewItem";
+import { ItemIndividual } from "./pages/ItemIndividual";
+import { UpdateItem } from "./pages/UpdateItem";
+
+export const router = createBrowserRouter([{
+  path: "/",
+  element: <RootLayout />,
+  children: [
+    { index: true, element: <Home /> },
+    { 
+      path: "/items", 
+      element: <ItemsLayout />,
+      children: [
+        { index: true, element: <ItemInStock /> },
+        { path: "/new", element: <NewItem /> },
+        { path: "/:id", element: <ItemIndividual /> },
+        { path: "/:id/update", element: <UpdateItem /> },
+      ]
+    }
+  ]
+}])
